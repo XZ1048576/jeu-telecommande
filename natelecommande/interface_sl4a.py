72f36bd1277a35ab66390e03bf1df3e8ec6fe18f
XZ1048576 <sylvain.lovis@gmail.com>
2018-02-25 17:52:52 +0100

    debogage joueur sl4a cote natel

--- natelecommande/interface_sl4a.py
+++ natelecommande/interface_sl4a.py
@@ -12,11 +12,13 @@ cnx=socket.socket()
 cnx.connect((addr[0],int(msg.split(b" ")[2])))
 while 1:
     msg=cnx.recv(1024)
+    if msg=b"\x82":
+        break
     choix=msg.split(b"\x81")
     titre=choix.pop(0)
-    droid.dialogCreateAlert(titre)
-    droid.dialogSetItems(choix)
-    droid.dialogShow
+    droid.dialogCreateAlert(titre.decode())
+    droid.dialogSetItems([x.decode() for x in choix])
+    droid.dialogShow()
     result=droid.dialogGetResponse().result
     if "item" in result:
         cnx.send(bytes([result["item"]]))
